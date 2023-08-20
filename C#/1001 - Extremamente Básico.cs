06c895af96786e398ac528b61bc4754dd69fefac
Leonardotei <111396420+Leonardotei@users.noreply.github.com>
2023-08-20 17:11:13 -0300

    Create  1001 - Extremamente Básico.cs

---
+++ C#/1001 - Extremamente Básico.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Soma{
+    class Program{
+        static void Main(string[] args){
+            int a, b, x;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            x = a + b;
+            
+            Console.WriteLine("X = " + x);
+        }
+    }
+}
