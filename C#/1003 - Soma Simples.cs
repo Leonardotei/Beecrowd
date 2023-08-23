3c01fb0bdd450d062e866ff123e352923d3c44e6
Leonardotei <111396420+Leonardotei@users.noreply.github.com>
2023-08-23 12:34:13 -0300

    Create 1003 - Soma Simples.cs

---
+++ C#/1003 - Soma Simples.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Soma{
+    class Program{
+        static void Main(string[] args){
+            int a, b, soma;
+            
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            soma = a + b;
+            
+            Console.WriteLine("SOMA = " + soma);
+        }
+    }
+}
